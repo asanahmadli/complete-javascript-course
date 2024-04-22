62c0bf57aaa090901ead7ac424421e2caf12dfb0
asanahmadli <asenahmedli@gmail.com>
2024-04-21 21:24:18 -0400

    Section 1 completed

---
+++ 01-Fundamentals-Part-1/starter/asan.js
@@ -0,0 +1,80 @@
+// console.log(61);
+
+// let firstName1 = "Matilda";
+// console.log(firstName1);
+// let $a = 10;
+// console.log($a);
+// console.log(typeof "asan");
+// let a;
+// console.log(a);
+// console.log(typeof a);
+// a = 10;
+// console.log(a);
+// let b = null;
+// console.log(b);
+// console.log(typeof b);
+
+// const age = 2093 - 1992;
+// const exampleNumber = 20;
+// console.log(age / 2, exampleNumber);
+
+// console.log(2 ** 3);
+
+// const firstName = "asan";
+// const lastName = "ahmadli";
+// console.log(firstName + " " + lastName);
+
+// let x = 10;
+// console.log(++x);
+
+// let a1, b1;
+// a1 = b1 = 10;
+// console.log(a1, b1);
+
+// let firstName = "asan";
+// let lastName = "ahmadli";
+// console.log(`${firstName}  ${lastName}`);
+// console.log(`asan ahmadli
+// nadir
+// `);
+
+// let birthYear = 2012;
+// let century;
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+
+// console.log(century);
+
+// const input = "1992";
+// const number = 12;
+// const str = "asan";
+// console.log(Number(input), input);
+// console.log(typeof String(number));
+// console.log("23" - "10" - 3);
+// console.log("23" + "10" + 3);
+// console.log("23" * "10" * 3);
+// console.log("23" / "10" / 3);
+
+/* 
+5 falsy values: 0,'',undefined,null,NaN
+*/
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(!undefined));
+// console.log(Boolean(null));
+// console.log(Boolean(NaN));
+
+// const favorite = prompt("what is your name?");
+// console.log(typeof favorite);
+
+// const hasDriverLicence = true;
+// const hasGoodVision = true;
+
+// console.log(!hasDriverLicence && hasGoodVision);
+
+const age = 10;
+let g = age > 10 ? "asan" : "coni";
+console.log(g);
---
+++ 01-Fundamentals-Part-1/starter/note.md
@@ -0,0 +1,25 @@
+keep variable names descriptive
+let and const
+in JS values can be object or can be primitive
+primitives
+number--used for decimals and integers
+string--sequence of characters
+boolean==logacl type true or false
+undefined--value taken by a variable that is not yet defined (empty value). typeof of the value is undefined and value as well undefined
+null--also means empty value
+biginit--larger integers than the Number type can hold
+JS has dynamic typing: We dont have to manually define the data type of the value stored in a variable. Instead, data types are determined automatically. Values has type not a variable.
+typeof operetor will give you type of the value
+console.log(typeof 'asan')
+string
+typeof null is object
+you cant reasign const variable
+convert string to number use Number()
+convert number to String use String()
+if you cant convert to number it gives you NaN. which type is number but invalid number
+in js only + contatinate strings rest of the operations automatically converts to numbers and make calculations.
+use triple equal compare strictly
+== type coercion
+expression is part of statement
+statement full sentence
+BABEL transpiling code back to previous version
